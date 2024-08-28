15f0d047c3178a872a2ee0616f06ac78d343280b
NattapolDedruktip <dedruktip.n@gmail.com>
2024-08-28 19:52:45 +0700

    update code 2

--- main.js
+++ main.js
@@ -1,51 +1,71 @@
 function App() {
-	const [counters, setCounters] = React.useState([
-		{ id: 1, number:9 },
-		{ id: 2, number:7 },
-		{ id: 3, number:2 },
-		{ id: 4, number:3 },
-		{ id: 5, number:5 },
-		{ id: 6, number:0 },
-	])
+	const [counters, setCounters] = React.useState([])
 
-	const updateCounter = (id ,n ) => {
-		console.log('id = ',id,)
-		let idx = counters.findIndex( el => el.id === id)
-		console.log('counters array no.', idx)
-		const newCounters = [...counters]
-        if(newCounters[idx].number <=0 && n == -1 ) return
-		newCounters[idx].number +=n
-		console.log(newCounters)
-		setCounters(newCounters)
+	let sum = counters.reduce( (accu , curr) =>{
+		return accu +  curr.number
+	},0 )
+  
+	function hdlClick(id, n) {
+		// console.log(id, n)
+		let index = counters.findIndex( item => item.id == id)
+		const newCounter = [...counters]
+		if( newCounter[index].number <1 && n == -1) {return}
+		newCounter[index].number += n
+		console.log(index , newCounter[index].number)
+		setCounters(newCounter)
+
+		
 	}
+
+	function hdlDelete(id){
+		console.log(111,id)
+		let index = counters.findIndex( item => item.id == id)
+		const newCounter = [...counters]
+		newCounter.splice(index,1)
+		setCounters(newCounter)
+
+	}
+
+	function addCounter(){
+		const newCounter = [...counters]
+		// console.log(newCounter)
+		let nextId = counters.length+1
+		newCounter.push({id:{nextId},number:0})
+		setCounters(newCounter)
+	}
+  
+  
 	return (
-		<div className='app'>
-			<h1 className="show-sum">Sum = 0 </h1>
-			<button className="btn-add">Add Counter</button>
-			<hr />
-			{ counters.map( el => (
-				<Counter key={el.id} item={el} updateCounter={updateCounter}/>
+	  <div>
+		<h1>Sum = {sum}</h1>
+		<button onClick={addCounter}>Add Counter</button>
+		<hr />
+		{
+			counters.map( (item) => (
+				<Counter key={item.id} item={item} hdlDelete={hdlDelete} hdlClick={hdlClick}/>
 			))
-
-			}
-		</div>
+		}
+	  </div>
 	)
-}
-
-function Counter(props) {
-	// console.log(props)
-	const {item, updateCounter} = props
-	return (
+  }   
+  
+  function Counter(props) {
+	  const { item, hdlClick , hdlDelete} = props
+	  return (
 		<div className="counter">
-			<button onClick={()=>updateCounter(item.id,-1)} className="btn btn-dec">-</button>
-			<h3 className="number">{item.number}</h3>
-			<button  onClick={()=>updateCounter(item.id,1)} className="btn btn-inc">+</button>
-			<button  onClick={()=>updateCounter(item.id,-item.number)} className="btn btn-clr">C</button>
+		  <button onClick={()=>hdlClick(item.id,-1)} className="btn btn-dec">-</button>
+		  <h3 >{item.number}</h3>
+		  <button onClick={()=>hdlClick(item.id,+1)} >+</button>
+		  <button onClick={()=>hdlClick(item.id,-item.number)} className="btn btn-clr">C</button>
+		  <button onClick={()=>hdlDelete(item.id)}>X</button>
 		</div>
-	)
-}
+	  )
+	}
 
-
-
-ReactDOM.createRoot(document.querySelector('#root'))
+  
+  
+  
+  
+  
+  ReactDOM.createRoot(document.querySelector('#root'))
 	.render(<App />)
